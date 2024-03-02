886a292f07011b15388f14594df50ccd42fabad1
Birkin James Diana <birkin.diana@gmail.com>
2024-03-02 16:27:09 -0500

    experiments with usage-statement.

--- src/nim_version_showing_commit.nim
+++ src/nim_version_showing_commit.nim
@@ -3,17 +3,21 @@ import std/parseopt
 
 ## import file created by `PRE_save_commit_hash.nim -----------------
 include version
-echo "\ninitial version: ", commit_hash
+# echo "\ninitial version: ", commit_hash
 
 
 ## load args from command line --------------------------------------
 var opt_parser = parseopt.initOptParser()
-echo "\nopt_parser: ", opt_parser
+# echo "\nopt_parser: ", opt_parser
 
+var kind = "init"
 for kind, key, val in opt_parser.getopt():
+    echo "\nin loop"
     echo "\nkind: ", kind, "; key: ", key, "; val: ", val
 
     if key == "version":
         echo "\nversion: ", commit_hash
     else:
-        echo "\nunknown key: ", key
+        echo "\nUsage: $ ./nim_version_showing_commit --version"
+
+echo "\nkind: ", kind
