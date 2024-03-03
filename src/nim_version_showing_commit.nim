779e8feba0c086e6ae7afcf9a305b6ad6e2ba37d
Birkin James Diana <birkin.diana@gmail.com>
2024-03-02 23:15:53 -0500

    implements logging.

--- src/nim_version_showing_commit.nim
+++ src/nim_version_showing_commit.nim
@@ -1,5 +1,7 @@
-import std/parseopt
 import std/logging
+import std/os
+import std/parseopt
+import std/tables
 
 
 ## import file created by `PRE_save_commit_hash.nim -----------------
@@ -7,8 +9,18 @@ include version
 
 
 ## set up logging -----------------------------------------------------
-var logger = newConsoleLogger( levelThreshold=lvlDebug )
-logger.log( lvlInfo, "a log message" )
+## grab level from envar ------------------------
+let appLogLevel: string = os.getEnv("APP_LOG_LEVEL")
+## get the level-obj ----------------------------
+var log_level_dict = {
+    "debug": lvlDebug,
+    "info": lvlInfo
+}.toTable()
+var level_obj = log_level_dict[appLogLevel]
+## instantiate logger ---------------------------
+var logger = newConsoleLogger( levelThreshold=level_obj )
+logger.log( lvlDebug, "level_obj, ", level_obj )
+# logger.log( lvlInfo, "an info-level log message" )
 
 
 ## load args from command line --------------------------------------
@@ -17,10 +29,13 @@ var
     versionFound: bool = false  # Flag to indicate whether --version is found
 for kind, key, val in opt_parser.getopt():
     assert kind is CmdLineKind and key is string and val is string
-    # echo "\nkind: ", kind, "; key: ", key, "; val: ", val
     logger.log( lvlDebug, "kind: ", kind, "; key: ", key, "; val: ", val )
+    ## display version --------------------------
     if kind == cmdLongOption and key == "version":
         echo "version: ", commit_hash
         versionFound = true
+## or show usage --------------------------------
 if not versionFound:
     echo "Usage: $ this_app --version"
+
+logger.log( lvlInfo, "program complete" )
